0a302b3de74bb96aaa6aca7fe9cad022ddfb55a6
rcompanocca <ronicompanocca@gmail.com>
2023-11-02 19:06:44 -0500

    ejercicios propuestos

--- Main.java
+++ Main.java
@@ -1,87 +1,266 @@
 // RONI COMPANOCCA CHECCO
 // CUI: 20210558
-// LABORATORIO 048
-import java.util.Scanner;
+// LABORATORIO 12
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        Ejercito ejercito1 = new Ejercito("Ejército 1");
-        Ejercito ejercito2 = new Ejercito("Ejército 2");
+		//DECLARACION DE VARIABLES Y ARREGLOS NECESARIOS
+		ArrayList<Soldado> ejercito1 = new ArrayList();
+		ArrayList<Soldado> ejercito2 = new ArrayList();
+		ArrayList<ArrayList<Soldado>> tablero = new ArrayList();
+		int batallon1, batallon2;
+		int vidatotal1=0, vidatotal2=0;
+		double promedioVida1=0, promedioVida2=0;
 
-        // Agregar soldados a los ejércitos (puedes hacerlo dinámicamente o con datos predefinidos)
-        ejercito1.agregarSoldado(new Soldado("Soldado1", 10, 5, 100, true));
-        ejercito1.agregarSoldado(new Soldado("Soldado2", 8, 6, 90, true));
-        ejercito2.agregarSoldado(new Soldado("Soldado3", 12, 4, 110, true));
-        ejercito2.agregarSoldado(new Soldado("Soldado4", 9, 7, 95, true));
+		// BUCLE PARA DESIGNAR LA CANTIDAD DE FILAS Y COLUMNAS DEL TABLERO
+		for(int i=0; i<10; i++) {
+			tablero.add(new ArrayList<Soldado>());
+			for(int j=0; j<10; j++) {
+				tablero.get(i).add(new Soldado());
+			}
+		}
 
-        Ejercito ejercitoSeleccionado = null;
-        boolean continuar = true;
+		// CREACION DEL NUMERO DE POSICIONES DE CADA EJERCITO
+		batallon1 = aleatorio(1,10);
+		batallon2 = aleatorio(1,10);
 
-        while (continuar) {
-            mostrarMenuPrincipal();
-            int opcion = scanner.nextInt();
+		// INICIALIZAR ARREGLOS
+		inicializarArreglo(ejercito1, batallon1);
+		inicializarArreglo(ejercito2, batallon2);
 
-            switch (opcion) {
-                case 1:
-                    // Crear Soldado
-                    if (ejercitoSeleccionado != null) {
-                        Soldado nuevoSoldado = crearSoldado(scanner);
-                        ejercitoSeleccionado.agregarSoldado(nuevoSoldado);
-                    } else {
-                        System.out.println("Primero selecciona un ejército.");
+		// GENERAR EJERCITOS VALIDOS
+		generarEjercitos(ejercito1, ejercito2);
+
+		// AÑADIR LOS EJERCITOS AL TABLERO
+		añadirTablero(ejercito1, tablero);
+		añadirTablero(ejercito2, tablero);
+
+		//IMPRIMIR EL TABLERO
+		imprimirTablero(tablero);
+
+		//IMPRIMIR LOS SOLDADOS DE MAYOR VIDA DE CADA EJERCITO
+		System.out.println("Soldado de mayor vida del ejercito 1");
+		SoldadoConMayorVida(ejercito1);
+		System.out.println("soldado de mayor vida del ejercito 2");
+		SoldadoConMayorVida(ejercito2);
+
+		//IMPRIMIR LA VIDA TOTAL Y EL PROMEDIO DEL EJERCITO 1
+		System.out.println("\nEJERCITO 1: ");
+		for (int i=0; i<ejercito1.size(); i++) {
+			vidatotal1+=ejercito1.get(i).getPuntos();
+			promedioVida1 = vidatotal1/(ejercito1.size()*1.0);
+		}
+		System.out.println("Vida total: "+vidatotal1);
+		System.out.println("Promedio de vida: "+promedioVida1);
+		
+		//IMPRIMIR LA VIDA TOTAL Y EL PROMEDIO DEL EJERCITO 2
+		System.out.println("\nEJERCITO 2: ");
+		for (int i=0; i<ejercito2.size(); i++) {
+			vidatotal2+=ejercito2.get(i).getPuntos();
+			promedioVida2 = vidatotal2/(ejercito2.size()*1.0);
+		}
+		System.out.println("Vida total: "+vidatotal2);
+		System.out.println("Promedio de vida: "+promedioVida2);
+
+		//IMPRIMIR LOS SOLDADOS CREADOS EN EL ORDEN POR DEFECTO
+		System.out.println("\nLista ejercito 1:");
+		for(int i=0; i<ejercito1.size(); i++) {
+			imprimir(ejercito1.get(i));
+		}
+		System.out.println("\nLista ejercito 2:");
+		for(int i=0; i<ejercito2.size(); i++) {
+			imprimir(ejercito2.get(i));
+		}
+
+		// IMPRIMIR LOS DATOS DE LOS SOLDADOS ORDENADOS DE MAYOR A MENOR DEPENDIENDO DE SU NIVEL DE VIDA USANDO DOS TIPOS DE ALGORITMO
+		ordenarPorVidaMetodoA(ejercito1);
+		ordenarPorVidaMetodoB(ejercito2);
+		System.out.println("\nEjercito 1 Ordenados por nivel de vida");
+		for(int i=0; i<ejercito1.size(); i++) {
+			imprimir(ejercito1.get(i));
+		}
+		System.out.println("\nEjercito 2 Ordenados por nivel de vida");
+		for(int i=0; i<ejercito2.size(); i++) {
+			imprimir(ejercito2.get(i));
+		}
+		
+		// MOSTRAR EJERCITO GANADOR LA METRICA USADA ARA DESIGNAR AL GANADOR ES POR EL NIVEL DEL PROMEDIO DE VIDA DE CADA EJERCITO
+		if(promedioVida1>promedioVida2) {
+			System.out.println("\nGANADOR ***EJERCITO 1***");
+		}else if (promedioVida1<promedioVida2) {
+			System.out.println("\nGANADOR ***EJERCITO 2***");
+			}
+		else {
+			System.out.print("\n***ES UN EMPATE***");
+		}
+	}
+
+    // METODO PARA CREAR NUMEROS ALEATORIOS EN UN RANGO
+    public static int aleatorio(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
+
+    // METODO PARA INICIAR UN ARRAYLIST
+    public static void inicializarArreglo(ArrayList<Soldado> soldadito, int num) {
+        for (int i = 0; i < num; i++) {
+            soldadito.add(new Soldado());
+        }
+    }
+
+    // METODO PARA GENERAR DATOS DEL OBJETO SOLDADO
+    public static Soldado generarDatos() {
+        Soldado soldadito = new Soldado();
+        soldadito.setNivelVida(aleatorio(1, 5));
+        soldadito.setFila(aleatorio(1, 10));
+        soldadito.setColumna(aleatorio(1, 10));
+        return soldadito;
+    }
+
+    // METODOS PARA GENERAR LOS EJERCITOS DE MANERA ALEATORIA
+    public static void generarEjercitos(ArrayList<Soldado> B1, ArrayList<Soldado> B2) {
+        ArrayList<Soldado> Soldados = new ArrayList<>();
+        Soldados.add(generarDatos());
+        for (int i = 1; i < (B1.size() + B2.size()); i++) {
+            Soldados.add(generarDatos());
+            for (int j = 0; j < i; j++) {
+                if (Soldados.get(i).getFila() == Soldados.get(j).getFila()) {
+                    if (Soldados.get(i).getColumna() == Soldados.get(j).getColumna()) {
+                        Soldados.remove(i);
+                        i--;
                     }
-                    break;
-                case 2:
-                    // Eliminar Soldado
-                    if (ejercitoSeleccionado != null) {
-                        ejercitoSeleccionado.eliminarSoldado(scanner);
-                    } else {
-                        System.out.println("Primero selecciona un ejército.");
-                    }
-                    break;
-                case 8:
-                    // Ver ejército
-                    if (ejercitoSeleccionado != null) {
-                        ejercitoSeleccionado.verEjercito();
-                    } else {
-                        System.out.println("Primero selecciona un ejército.");
-                    }
-                    break;
-                case 12:
-                    // Salir
-                    continuar = false;
-                    break;
-                default:
-                    System.out.println("Opción no válida. Por favor, elige una opción del 1 al 12.");
-                    break;
+                }
             }
         }
-        scanner.close();
+        for (int i = 0; i < B1.size(); i++) {
+            B1.add(i, Soldados.get(i));
+            B1.get(i).setNombre("Soldado" + i + "x1");
+            B1.get(i).setActitud(B1.get(i).getNivelVida() + "[E1]");
+            B1.remove(i + 1);
+        }
+        for (int i = 0; i < B2.size(); i++) {
+            B2.add(i, Soldados.get(i + B1.size()));
+            B2.get(i).setNombre("Soldado" + i + "x2");
+            B2.remove(i + 1);
+            B2.get(i).setActitud(B2.get(i).getNivelVida() + "[E2]");
+        }
     }
 
-    private static void mostrarMenuPrincipal() {
-        System.out.println("Seleccione una opción:");
-        System.out.println("1) Crear Soldado");
-        System.out.println("2) Eliminar Soldado");
-        System.out.println("8) Ver ejército");
-        System.out.println("12) Salir");
+    // METODO PARA AÑADIR LOS EJERCITOS AL TABLERO
+    public static void añadirTablero(ArrayList<Soldado> soldadito, ArrayList<ArrayList<Soldado>> table) {
+        for (int i = 0; i < soldadito.size(); i++) {
+            table.get(soldadito.get(i).getColumna() - 1).add(soldadito.get(i).getFila() - 1, soldadito.get(i));
+            table.get(soldadito.get(i).getColumna() - 1).remove(soldadito.get(i).getFila());
+        }
     }
 
-    private static Soldado crearSoldado(Scanner scanner) {
-        System.out.println("Ingresa los detalles del nuevo soldado:");
-        System.out.print("Nombre: ");
-        String nombre = scanner.next();
-        System.out.print("Nivel de Ataque: ");
-        int nivelAtaque = scanner.nextInt();
-        System.out.print("Nivel de Defensa: ");
-        int nivelDefensa = scanner.nextInt();
-        System.out.print("Vida Actual: ");
-        int vidaActual = scanner.nextInt();
-        System.out.print("¿Vive? (true/false): ");
-        boolean vive = scanner.nextBoolean();
+    // METODO PARA IMPRIMIR EL TABLERO EN LA CUAL SE DESARROLLA EL JUEGO
+    public static void imprimirTablero(ArrayList<ArrayList<Soldado>> table) {
+        System.out.println("\tA\tB\tC\tD\tF\tG\tH\tI\tJ");
+        for (int i = 0; i < table.size(); i++) {
+            System.out.print(i + 1);
+            for (int j = 0; j < table.get(i).size(); j++) {
+                System.out.print("\t" + table.get(i).get(j).getActitud());
+            }
+            System.out.println("\n");
+        }
+    }
 
-        return new Soldado(nombre, nivelAtaque, nivelDefensa, vidaActual, vive);
+    // METODO PARA IMPRIMIR LOS SOLDADOS DE MAYOR VIDA
+    public static void SoldadoConMayorVida(ArrayList<Soldado> soldadito) {
+        Soldado mayor = new Soldado();
+        mayor.setNivelVida(0);
+        for (int i = 0; i < soldadito.size(); i++) {
+            if (mayor.getNivelVida() < soldadito.get(i).getNivelVida()) {
+                mayor = soldadito.get(i);
+            }
+        }
+        imprimir(mayor);
+    }
+
+    // METODO PARA IMPRIMIR EL NOMBRE, LA POSICION Y NIVEL DE VIDA DEL SOLDADO
+    public static void imprimir(Soldado soldadito) {
+        System.out.println("Nombre: " + soldadito.getNombre() + "\nPosicion: " + soldadito.getColumna() + "X" + soldadito.getFila() + "\tVida: " + soldadito.getNivelVida());
+    }
+
+    // METODO QUE NOS AYUDA A ORDENAR LOS SOLDADOS DE ACUERDO A SU NIVEL DE VIDA, USUANDO UN ALGORITMO DE ORDENAMIENTO DE BURBUJA
+    public static void ordenarPorVidaMetodoA(ArrayList<Soldado> soldadito) {
+        Soldado aux = new Soldado();
+        for (int i = 0; i < soldadito.size() - 1; i++) {
+            for (int j = 0; j < soldadito.size() - i - 1; j++) {
+                if (soldadito.get(j).getNivelVida() < soldadito.get(j + 1).getNivelVida()) {
+                    aux = soldadito.get(j);
+                    soldadito.set(j, soldadito.get(j + 1));
+                    soldadito.set(j + 1, aux);
+                }
+            }
+        }
+    }
+
+    // METODO QUE NOS AYUDA A ORDENAR LOS SOLDADOS DE ACUERDO A SU NIVEL DE VIDA, EN ESTA OCACION DIFERENTE A LA ANTERIOR QUE ERA ALGORITMO DE BURBUJA
+    public static void ordenarPorVidaMetodoB(ArrayList<Soldado> soldadito) {
+        Collections.sort(soldadito, new Comparator<Soldado>() {
+            public int compare(Soldado s1, Soldado s2) {
+                // Orden descendente por nivel de vida
+                return Integer.compare(s2.getNivelVida(), s1.getNivelVida());
+            }
+        });
+    }
+}
+    // METODO PARA IMPRIMIR EL TABLERO EN LA CUAL SE DESARROLLA EL JUEGO
+    public static void imprimirTablero(ArrayList<ArrayList<Soldado>> table) {
+        System.out.println("\tA\tB\tC\tD\tF\tG\tH\tI\tJ");
+        for (int i = 0; i < table.size(); i++) {
+            System.out.print(i + 1);
+            for (int j = 0; j < table.get(i).size(); j++) {
+                System.out.print("\t" + table.get(i).get(j).getActitud());
+            }
+            System.out.println("\n");
+        }
+    }
+
+    // METODO PARA IMPRIMIR LOS SOLDADOS DE MAYOR VIDA
+    public static void SoldadoConMayorVida(ArrayList<Soldado> soldadito) {
+        Soldado mayor = new Soldado();
+        mayor.setNivelVida(0);
+        for (int i = 0; i < soldadito.size(); i++) {
+            if (mayor.getNivelVida() < soldadito.get(i).getNivelVida()) {
+                mayor = soldadito.get(i);
+            }
+        }
+        imprimir(mayor);
+    }
+
+    // METODO PARA IMPRIMIR EL NOMBRE, LA POSICION Y NIVEL DE VIDA DEL SOLDADO
+    public static void imprimir(Soldado soldadito) {
+        System.out.println("Nombre: " + soldadito.getNombre() + "\nPosicion: " + soldadito.getColumna() + "X" + soldadito.getFila() + "\tVida: " + soldadito.getNivelVida());
+    }
+
+    // METODO QUE NOS AYUDA A ORDENAR LOS SOLDADOS DE ACUERDO A SU NIVEL DE VIDA, USUANDO UN ALGORITMO DE ORDENAMIENTO DE BURBUJA
+    public static void ordenarPorVidaMetodoA(ArrayList<Soldado> soldadito) {
+        Soldado aux = new Soldado();
+        for (int i = 0; i < soldadito.size() - 1; i++) {
+            for (int j = 0; j < soldadito.size() - i - 1; j++) {
+                if (soldadito.get(j).getNivelVida() < soldadito.get(j + 1).getNivelVida()) {
+                    aux = soldadito.get(j);
+                    soldadito.set(j, soldadito.get(j + 1));
+                    soldadito.set(j + 1, aux);
+                }
+            }
+        }
+    }
+
+    // METODO QUE NOS AYUDA A ORDENAR LOS SOLDADOS DE ACUERDO A SU NIVEL DE VIDA, EN ESTA OCACION DIFERENTE A LA ANTERIOR QUE ERA ALGORITMO DE BURBUJA
+    public static void ordenarPorVidaMetodoB(ArrayList<Soldado> soldadito) {
+        Collections.sort(soldadito, new Comparator<Soldado>() {
+            public int compare(Soldado s1, Soldado s2) {
+                // Orden descendente por nivel de vida
+                return Integer.compare(s2.getNivelVida(), s1.getNivelVida());
+            }
+        });
     }
 }
--- Soldado.java
+++ Soldado.java
@@ -1,21 +1,92 @@
 // RONI COMPANOCCA CHECCO
 // CUI: 20210558
-// LABORATORIO 04
+// LABORATORIO 12
 // FUNDAMENTOS DE PROGRAMACION 
 // CLASE SOLDADO PARA LOS METODOS SETTER Y GETTER
-class Soldado {
+public class Soldado {
     private String nombre;
     private int nivelAtaque;
     private int nivelDefensa;
+    private int nivelVida;
     private int vidaActual;
+    private int velocidad;
+    private String actitud;
     private boolean vive;
 
-    public Soldado(String nombre, int nivelAtaque, int nivelDefensa, int vidaActual, boolean vive) {
-        this.nombre = nombre;
-        this.nivelAtaque = nivelAtaque;
-        this.nivelDefensa = nivelDefensa;
+    public Soldado() {
+        nombre = "";
+        nivelAtaque = 0;
+        nivelDefensa = 0;
+        nivelVida = 0;
+        vidaActual = nivelVida;
+        velocidad = 0;
+        actitud = "";
+        vive = true;
+    }
+
+    public void atacar(Soldado enemigo) {
+        int danio = this.nivelAtaque - enemigo.nivelDefensa;
+        if (danio > 0) {
+            enemigo.serAtacado(danio);
+            System.out.println(this.nombre + " atacó a " + enemigo.nombre + " y le causó " + danio + " de daño.");
+        } else {
+            System.out.println(this.nombre + " atacó a " + enemigo.nombre + " pero no le causó daño.");
+        }
+    }
+
+    public void defender() {
+        // Lógica para la defensa
+        // Puede disminuir el daño recibido en un futuro ataque, por ejemplo
+        this.nivelDefensa += 10; // Aumentar la defensa por ejemplo
+        System.out.println(this.nombre + " se está defendiendo.");
+    }
+
+    public void avanzar() {
+        // Lógica para avanzar en el juego
+        // Podría mover al soldado a una nueva posición en el tablero, por ejemplo
+        if (this.velocidad > 0) {
+            // Mover al soldado en la dirección correspondiente
+            System.out.println(this.nombre + " está avanzando.");
+        } else {
+            System.out.println(this.nombre + " no puede avanzar, la velocidad es 0.");
+        }
+    }
+
+    public void retroceder() {
+        // Lógica para retroceder en el juego
+        // Podría mover al soldado hacia atrás en el tablero, por ejemplo
+        System.out.println(this.nombre + " está retrocediendo.");
+    }
+
+    public void serAtacado(int danioRecibido) {
+        this.vidaActual -= danioRecibido;
+        if (this.vidaActual <= 0) {
+            this.morir();
+        }
+    }
+
+    public void huir() {
+        // Lógica para huir del combate
+        // Podría cambiar la posición del soldado a una zona segura, por ejemplo
+        if (this.nivelVida < 10) {
+            // Huir solo si la vida es baja
+            System.out.println(this.nombre + " está huyendo del combate.");
+        } else {
+            System.out.println(this.nombre + " no puede huir, su vida está alta.");
+        }
+    }
+
+    public void morir() {
+        this.vidaActual = 0;
+        this.vive = false;
+    }
+
+    public void setVidaActual(int vidaActual) {
         this.vidaActual = vidaActual;
-        this.vive = vive;
+    }
+
+    public int getVidaActual() {
+        return vidaActual;
     }
 
     // Getters y Setters
@@ -24,12 +95,20 @@ class Soldado {
         return nombre;
     }
 
+    public String getNivelVida() {
+        return nivelVida;
+    }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     public int getNivelAtaque() {
         return nivelAtaque;
+    }
+
+    public int setNivelVida() {
+        this.nivelVida = nivelVida;
     }
 
     public void setNivelAtaque(int nivelAtaque) {
@@ -42,14 +121,6 @@ class Soldado {
 
     public void setNivelDefensa(int nivelDefensa) {
         this.nivelDefensa = nivelDefensa;
-    }
-
-    public int getVidaActual() {
-        return vidaActual;
-    }
-
-    public void setVidaActual(int vidaActual) {
-        this.vidaActual = vidaActual;
     }
 
     public boolean isVive() {
